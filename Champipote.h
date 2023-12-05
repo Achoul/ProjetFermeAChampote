d06927de6063bb15a2ea5c8ee8f4aa5d207e4c42
Chichou <hcaute@gmail.com>
2023-12-05 10:45:13 +0100

    creation driver capteur d'eau compile

---
+++ Champipote.h
@@ -0,0 +1,20 @@
+
+#ifndef __Champipote
+#define __Champipote
+
+#include "PowerOutput.h"
+#include "Service_DHT11.h"
+#include "Service_Hum.h"
+#include "Service_Temp.h"
+#include "Blue_Button.h"
+
+class Champipote{
+
+
+//fonctions a réfléchirrrrrrrr
+
+};
+
+
+
+#endif
